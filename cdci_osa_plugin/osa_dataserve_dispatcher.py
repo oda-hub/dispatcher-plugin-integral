450c996fdad69832f0c2df8c09e725161d7207d3
andrea tramacere <orion@MacBook-Pro-3.local>
2019-12-06 12:52:36 +0100

    testing scwlist len

--- cdci_osa_plugin/osa_dataserve_dispatcher.py
+++ cdci_osa_plugin/osa_dataserve_dispatcher.py
@@ -563,8 +563,9 @@ class OsaDispatcher(object):
     @classmethod
     def get_scwlist_assumption(cls, scw_list, T1, T2, RA, DEC, radius, use_max_pointings):
 
+        print('DEBUG --> scw_list', scw_list,len(scw_list))
         if scw_list is not None and scw_list != []:
-            #print('DEBUG --> scw_list', scw_list)
+
             scw_list=[item.strip() for item in scw_list]
             template = re.compile(r'^(\d{12}).(\d{3})$')
             acceptList = [item.strip() for item in scw_list if template.match(item)]
