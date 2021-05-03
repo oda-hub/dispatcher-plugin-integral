742fd563f61727f8d640bfa01f75321c26e4c295
burnout87 <gabriele.barni@gmail.com>
2021-05-03 18:50:26 +0200

    jemx tests

--- tests/test_jemx_plugin.py
+++ tests/test_jemx_plugin.py
@@ -5,23 +5,37 @@ import json
 
 logger = logging.getLogger(__name__)
 
-default_params = dict(
-    query_status="new",
-    query_type="Real",
-    instrument="jemx",
-    product_type="antares_spectrum",
-    RA=280.229167,
-    DEC=-5.55,
-    radius=2.,
-    index_min=1.5,
-    index_max=3.,
-    async_dispatcher=False
-)
+default_params = dict(instrument='jemx',
+                        jemx_num='2',
+                        product_type='jemx_image',
+                        scw_list=['010200230010.001'],
+                        osa_version='OSA10.2',
+                        detection_threshold=5.0,
+                        radius=15.,
+                        query_type='dummy')
 
-dummy_params = dict(
-    query_status="new",
-    query_type="Dummy",
-    instrument="isgri",
-    product_type="isgri_image",
-    async_dispatcher=False
-)
+
+def test_default(dispatcher_live_fixture):
+    server = dispatcher_live_fixture
+
+
+@pytest.mark.parametrize("product_type", ['jemx_spectrum', 'jemx_image', 'jemx_spectrum'])
+@pytest.mark.depends(on=['test_default'])
+def test_jemx_dummy(dispatcher_live_fixture, product_type):
+    server = dispatcher_live_fixture
+
+    params = {
+        **default_params,
+        "product_type": product_type,
+        "query_type": 'real'
+    }
+
+    logger.info("constructed server: %s", server)
+    c = requests.get(server + "/run_analysis",
+                      params=params)
+    logger.info("content: %s", c.text)
+    jdata = c.json()
+    logger.info(list(jdata.keys()))
+    logger.info(jdata)
+    assert c.status_code == 200
+    assert jdata['job_status'] == 'done'
