0d280a442bef4101c78971e48a62202ade9d46d5
burnout87 <gabriele.barni@gmail.com>
2021-06-28 18:43:03 +0200

    mock_dda_server and test using the mock data server for isgri instrument

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,8 @@ from cdci_data_analysis.pytest_fixtures import (
         )
 
 import pytest
+import os
+import yaml
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -28,3 +30,78 @@ def pytest_collection_modifyitems(config, items):
         if "dda" in item.keywords:
             item.add_marker(skip_slow)
 
+
+@pytest.fixture
+def test_data_server_conf_fn():
+    env_conf_file_path = os.environ.get('CDCI_OSA_PLUGIN_CONF_FILE')
+    if env_conf_file_path is not None:
+        folder_path = os.path.dirname(env_conf_file_path)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        with open(env_conf_file_path, "w") as f:
+            f.write("""
+    instruments:
+      isgri:
+          dispatcher_mnt_point: .
+          data_server_cache: reduced/ddcache
+          # this is for now, for testing purposes, but an appropriate fixture should be defined
+          dummy_cache: data/dummy_prods
+          data_server_url: http://localhost:1234
+          data_server_port:
+    
+      jemx:
+          dispatcher_mnt_point: .
+          data_server_cache: reduced/ddcache
+          dummy_cache: data/dummy_prods_jemx
+          data_server_port:
+          data_server_url: http://localhost:1234
+    
+        """)
+
+    yield env_conf_file_path
+
+
+@pytest.fixture
+def mock_isgri_dda_server(test_data_server_conf_fn):
+
+    start_mock_dda_server('isgri', test_data_server_conf_fn)
+
+    yield test_data_server_conf_fn
+
+
+@pytest.fixture
+def mock_jemx_dda_server(test_data_server_conf_fn):
+    start_mock_dda_server('jemx', test_data_server_conf_fn)
+
+
+def start_mock_dda_server(instrument, config_path):
+    data_server_url = None
+
+    with open(config_path, 'r') as ymlfile:
+        test_config = yaml.load(ymlfile)
+
+    if test_config is not None:
+        data_server_url = test_config['instruments'][instrument]['data_server_url']
+
+    if data_server_url is not None:
+        from flask import Flask
+        from raven.utils.urlparse import urlparse
+        from threading import Thread
+
+        parse_result = urlparse(data_server_url)
+
+        app = Flask(__name__)
+
+        @app.route("/api/v2.0/poke", methods=['POST', 'GET'])
+        def pokev2():
+            return "Works! api v2.0"
+
+        @app.route("/api/v1.0/poke", methods=['POST', 'GET'])
+        def pokev1():
+            return "Works! api v1.0"
+
+        def mock_flask_thread():
+            app.run(host=parse_result.hostname, port=parse_result.port)
+
+        thread = Thread(target=mock_flask_thread, args=())
+        thread.start()
