c716b656ce008307da45cb9e651db221a4e7876b
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-10-05 09:42:12 +0200

    more log on bad version

--- cdci_osa_plugin/osa_common_pars.py
+++ cdci_osa_plugin/osa_common_pars.py
@@ -140,6 +140,7 @@ class OSAVersion(Name):
             if osa_subversion != 'default-isdc':
                 # suggestions should be only given to users with special roles. Let's just give none
                 if f"{osa_version_base}-{osa_subversion}" not in self._allowed_values:
+                    logger.warning("unknown dev OSA version %s, allowed %s", f"{osa_version_base}-{osa_subversion}", self._allowed_values)
                     raise RuntimeError("unknown dev OSA version!")
 
             if isinstance(v, (str, six.string_types)):
