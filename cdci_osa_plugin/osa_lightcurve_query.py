9c4017c16b023367815abf0563d9b205522c3140
Denys Savchenko <56398430+dsavchenko@users.noreply.github.com>
2026-03-10 15:34:34 +0100

    Allow XAX_E column and TIMEDEL from header in isgri lc get_html_draw (#166)
    * Allow XAX_E column and TIMEDEL from header in isgri lc get_html_draw
    
    * no get

--- cdci_osa_plugin/osa_lightcurve_query.py
+++ cdci_osa_plugin/osa_lightcurve_query.py
@@ -243,12 +243,21 @@ class OsaLightCurve(LightCurveProduct):
         data = data[msk_non_zero]
 
         x = data['TIME']
-        dx = data['TIMEDEL']*0.5
+        colnames = [x.name for x in data.columns]
+        if 'TIMEDEL' in colnames:
+            dx = data['TIMEDEL'] / 2
+        elif 'XAX_E' in colnames:
+            dx = data['XAX_E']
+        elif 'TIMEDEL' in header.keys():
+            dx = float(header['TIMEDEL'])*np.ones(len(x)) / 2
+        else:
+            raise RuntimeError(f'no information on time bin length is available through TIMEDEL or XAX_E when plotting lightcurve for {self.name}')
+        
         y = data['RATE']
         dy = data['ERROR']
         try:
             mjdref = header['mjdref'] + int(x.min())
-        except:
+        except KeyError:
             mjdref = header['MJDREF'] + int(x.min())
 
         x = x - int(x.min())
